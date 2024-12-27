9dcdb61d3cd14940d5e8d15622fbcd0e4bca8652
John C Harrison <john@johnsmacbook.lan>
2024-12-26 21:35:06 -0500

    changed some things

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,32 @@
-<script>import "./app.css";
-import svelteLogo from './assets/svelte.svg'
-import viteLogo from '/vite.svg'
-import Counter from './lib/Counter.svelte'</script>
+<script>
+  import "./app.css";
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+    DarkMode,
+  } from "flowbite-svelte";
+</script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src="{viteLogo}" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<div>
+  <Navbar fluid>
+    <NavBrand href="/">
+      <span
+        class="self-center whitespace-nowrap text-4xl font-semibold dark:text-green-500 text-green-800"
+        >ECHO STATS</span
+      >
+    </NavBrand>
+    <NavHamburger />
+    <NavUl>
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+      <NavLi href="/contact">Contact</NavLi>
+      <NavLi><DarkMode /></NavLi>
+    </NavUl>
+  </Navbar>
 
-  <div class="card">
-    <Counter></Counter>
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+  <div></div>
+</div>
